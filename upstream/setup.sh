e0de7eb8b3509e1a503d1164ef3b5e4ee2adcfef
Kristaps Čivkulis <kristaps.civkulis@gmail.com>
2022-04-12 15:27:14 +0300

    Upgrade to newest stag-mwc

--- upstream/setup.sh
+++ upstream/setup.sh
@@ -5,27 +5,7 @@ cd ../..
 rm -rf stag-mwc
 
 # first thing's first clone stag if it's not here already
-git clone https://github.com/kcivkulis/stag-mwc stag-mwc
+git clone https://github.com/ctmrbio/stag-mwc
 
-# go into the folder and pull changes (mostly to deal with a case where the repohas already been pulled)
-cd stag-mwc
-git checkout hpc_mod # the modded branch that we're working with
-git pull
-
-# clear out the old installation folders
-cd ..
-rm -rf kraken2
-
-# we need to download and compile kraken2 to avoid using the segfaulting conda version
-#
-# remeber that build-essential have to be installed (at least in the wsl ubuntu build)
-# apt-get install build-essential
-git clone https://github.com/DerrickWood/kraken2.git kraken2
-cd kraken2
-./install_kraken2.sh ../kraken2_installed
-# get rid of the repo folder and move the installation dir to a nicer looking folder name
-cd ..
-rm -rf kraken2
-mv kraken2_installed kraken2
-
+# Clone Bracken
 git clone https://github.com/jenniferlu717/Bracken
